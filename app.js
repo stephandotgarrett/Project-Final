ff3a4b2d2ad0d2a4e16f5eb09ccc5aab18d0388b
Stephan Garrett <stephan.garrett@yahoo.com>
2019-03-04 03:28:02 -0500

    removed from nav style

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-if(window.matchMedia("(min-width: 766px)").matches){
-
-  $('#navBar').removeClass('fixed-bottom')
-  $('#navBar').addClass('fixed-top')
-
-}
+// if(window.matchMedia("(min-width: 766px)").matches){
+//
+//   $('#navBar').removeClass('fixed-bottom')
+//   $('#navBar').addClass('fixed-top')
+//
+// }
